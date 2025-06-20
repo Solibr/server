ca41d6abfba37f2c69b1af79b9a353bc2b9ce112
Arvoglade <arvoglade@gmail.com>
2025-06-20 17:52:07 +0500

    Database connection

---
+++ src/main/java/com/example/server/Account.java
@@ -0,0 +1,24 @@
+package com.example.server;
+
+import java.math.BigDecimal;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "accounts")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Account {
+    @Id
+    private Long id;
+    private BigDecimal amount;
+    private String name;
+}
---
+++ src/main/resources/db/changelog/2025-06/00-initial.sql
@@ -0,0 +1,5 @@
+create table accounts (  
+    id bigint primary key,
+    amount numeric(20,2),
+    name varchar  
+); 
